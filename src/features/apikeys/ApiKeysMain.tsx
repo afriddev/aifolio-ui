ec8b33c24012ee8c27e02f0966d4aac7b6265913
afriddev <afridayan01@gmail.com>
2025-10-07 12:35:27 +0530

    feat: enhance ApiKeysMain component; add alert for copied API key and improve API key status display

--- src/features/apikeys/ApiKeysMain.tsx
+++ src/features/apikeys/ApiKeysMain.tsx
@@ -10,19 +10,26 @@ import {
 import { MdOutlineDeleteOutline } from "react-icons/md";
 import { IoIosRemoveCircleOutline } from "react-icons/io";
 import { IoMdCheckmarkCircleOutline } from "react-icons/io";
-import { CiCircleAlert } from "react-icons/ci";
+import { BiError } from "react-icons/bi";
 import { RiLoopLeftFill } from "react-icons/ri";
 import { MdDataSaverOff } from "react-icons/md";
 import { useGetAllApiKeys } from "@/hooks/ApiKeyHooks";
 import { useEffect, useState } from "react";
 import type { apiKeyDataType } from "@/types/ApiKeysDataTypes";
 import { getFormattedDate, maskApiKey } from "@/apputils/AppUtils";
+import { IoCheckmark } from "react-icons/io5";
+import { MdDoNotDisturb } from "react-icons/md";
+import { FaRegCircleXmark } from "react-icons/fa6";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import { Terminal } from "lucide-react";
+import { IoCheckmarkCircleOutline } from "react-icons/io5";
 
 function ApiKeysMain() {
   const [apiKeys, setApiKeys] = useState<apiKeyDataType[] | undefined>(
     undefined
   );
   const { getApiKeys } = useGetAllApiKeys();
+  const [copied, setCopied] = useState<boolean>(false);
 
   useEffect(() => {
     if (apiKeys === undefined) {
@@ -48,8 +55,15 @@ function ApiKeysMain() {
               <Button>Generate API Key</Button>
             </div>
           </div>
+
+          {copied && (
+            <Alert className="mt-5" variant="constructive">
+              <IoCheckmarkCircleOutline className="w-5 h-5" />
+              <AlertTitle>Copied API key to clipboard.</AlertTitle>
+            </Alert>
+          )}
           <div className="mt-6 text-foreground/80">
-            <p>These API keys allow you to access data for your chat bot </p>
+            <p>These API keys provide programmatic access to your data through the AiFolio library. </p>
           </div>
           <div className="font-semibold flex shadow rounded-tr-lg rounded-tl-lg mt-5  bg-white p-4 items-center justify-between">
             <div className="w-[10vw] ">Name</div>
@@ -64,26 +78,48 @@ function ApiKeysMain() {
                 key={index}
                 className=" flex shadow rounded-br-lg rounded-tbl-lg mt-[1px]  bg-white p-4 items-center justify-between"
               >
-                <div className="w-[10vw]  font-semibold">{key.name}</div>
+                <div className="w-[10vw]  font-semibold">
+                  {key.status === "ERROR" ? "-" : key.name}
+                </div>
                 <div className="w-[15vw] font-light flex items-center gap-3">
                   {maskApiKey(key.key)}
-                  <LuCopy className="size-4 cursor-pointer  " />
+                  {copied ? (
+                    <IoCheckmarkCircleOutline
+                      aria-label="Copied"
+                      className="size-4 text-constructive"
+                    />
+                  ) : (
+                    <LuCopy
+                      aria-label="Copy"
+                      onClick={async () => {
+                        await navigator.clipboard.writeText(
+                          key.key ? key.key : ""
+                        );
+                        setCopied(true);
+                        setTimeout(() => setCopied(false), 3000);
+                      }}
+                      className="size-4 cursor-pointer  "
+                    />
+                  )}
                 </div>
                 <div className="w-[10vw] font-light">
                   {getFormattedDate(key.createdAt)}
                 </div>
                 {key.status === "ACTIVE" ? (
-                  <div className="w-[10vw] font-light flex items-center gap-1">
+                  <div className="w-[10vw] font-semibold text-constructive flex items-center gap-1">
                     Active
-                    <MdVerified className="text-constructive" />
+                    <MdVerified className="" />
                   </div>
                 ) : key.status === "PENDING" ? (
-                  <div className="w-[10vw] font-light flex items-center gap-1">
+                  <div className="w-[10vw] font-semibold text-orange-500 flex items-center gap-1">
                     Pending
-                    <CiCircleAlert className="text-destructive" />
+                    <BiError className="" />
                   </div>
                 ) : (
-                  <></> 
+                  <div className="w-[10vw] font-semibold text-destructive flex items-center gap-1">
+                    Error
+                    <FaRegCircleXmark className="text-destructive" />
+                  </div>
                 )}
 
                 <div className="w-[5vw] font-light">
@@ -93,21 +129,23 @@ function ApiKeysMain() {
                     </PopoverTrigger>
                     <PopoverContent className="p-1 w-32  m-0">
                       <div className="flex flex-col gap-1">
-                        <Button
-                          variant={"ghost"}
-                          className="w-full flex  justify-start"
-                        >
-                          <IoIosRemoveCircleOutline className="size-4 text-destructive" />
-                          Disable
-                        </Button>
-
-                        <Button
-                          variant={"ghost"}
-                          className="w-full flex  justify-start"
-                        >
-                          <IoMdCheckmarkCircleOutline className="size-4 text-constructive" />
-                          Enable
-                        </Button>
+                        {key.disabled ? (
+                          <Button
+                            variant={"ghost"}
+                            className="w-full flex  justify-start"
+                          >
+                            <IoMdCheckmarkCircleOutline className="size-4 text-constructive" />
+                            Enable
+                          </Button>
+                        ) : (
+                          <Button
+                            variant={"ghost"}
+                            className="w-full flex  justify-start"
+                          >
+                            <MdDoNotDisturb className="size-4 text-destructive" />
+                            Disable
+                          </Button>
+                        )}
 
                         <Button
                           variant={"ghost"}
@@ -117,13 +155,15 @@ function ApiKeysMain() {
                           Data
                         </Button>
 
-                        <Button
-                          variant={"ghost"}
-                          className="w-full flex  justify-start"
-                        >
-                          <RiLoopLeftFill className="size-4 " />
-                          Retry
-                        </Button>
+                        {key.status === "ERROR" && (
+                          <Button
+                            variant={"ghost"}
+                            className="w-full flex  justify-start"
+                          >
+                            <RiLoopLeftFill className="size-4 " />
+                            Retry
+                          </Button>
+                        )}
                         <Button
                           variant={"ghost"}
                           className="w-full flex  justify-start"
--- src/types/apiKeysDataTypes.ts
+++ src/types/apiKeysDataTypes.ts
@@ -2,7 +2,7 @@ export type apiKeyDataType = {
   id: string;
   name: string;
   key: string;
-  status: "DISABLED" | "ACTIVE" | "PENDING";
+  status: "DISABLED" | "ACTIVE" | "PENDING" | "ERROR";
   deleted: boolean;
   disabled: boolean;
   createdAt: string;
