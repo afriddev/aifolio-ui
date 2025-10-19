f06fd0290553fcf1e6c7761598e71b010d4a9427
afriddev <afridayan01@gmail.com>
2025-10-20 01:31:31 +0530

    Implement code changes to enhance functionality and improve performance

--- src/features/home/HomeMain.tsx
+++ src/features/home/HomeMain.tsx
@@ -5,23 +5,31 @@ function HomeMain() {
   return (
     <div className="h-full w-full flex items-center justify-center">
       <div className="w-[70%] flex justify-center">
-        <div className=" flex flex-col w-full gap-10 min-h-[90vh]   items-center justify-center h-full">
-          <div className="flex justify-center gap-1  items-center ">
-            <GoDependabot className="h-8 w-8  " />
-            <h3 className="text-4xl ">Chatbots</h3>
-            <div className="border ml-2 border-foreground  rounded-xs  px-2  h-8  flex items-center justify-center text-lg">
-              Beta
+        <div className="flex ">
+          <img
+            src="home-bg-01.png"
+            className="w-[60vw] h-[120vh]  -ml-[10vw]"
+          />
+          <div className=" flex flex-col w-full gap-10    min-h-[90vh]   items-center justify-center h-full">
+            <div className="flex justify-center gap-1  -mt-[40vh] items-center ">
+              <GoDependabot className="h-8 w-8  " />
+              <h3 className="text-4xl ">Chatbots</h3>
+              <div className="border ml-2 border-foreground  rounded-xs  px-2  h-8  flex items-center justify-center text-lg">
+                Beta
+              </div>
             </div>
+            <h1 className=" text-[80px] text-center -mt-10">
+              Empower Your Knowledge with AI
+            </h1>
+            <p className="text-2xl w-[70%] text-center">
+              Upload documents or YouTube videos, get instant answers, and
+              access multiple AI models. Manage API keys, track usage, and
+              integrate smart AI features seamlessly into your projects.
+            </p>
+            <Button className="text-xl px-10 py-3  rounded">
+              Start Building
+            </Button>
           </div>
-          <h1 className=" text-[80px] -mt-10">
-            Empower Your Knowledge with AI
-          </h1>
-          <p className="text-2xl w-[70%] text-center">
-            Upload documents or YouTube videos, get instant answers, and access
-            multiple AI models. Manage API keys, track usage, and integrate
-            smart AI features seamlessly into your projects.
-          </p>
-          <Button className="text-xl px-10 py-3  rounded">Start Building</Button>
         </div>
       </div>
     </div>
