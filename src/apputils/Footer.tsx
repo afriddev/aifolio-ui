16e45c38cadc6e4514bd765da542b42fd0d3768f
afriddev <afridayan01@gmail.com>
2025-10-20 00:45:53 +0530

    fix: update Footer component layout and navigation links for improved accessibility and consistency

--- src/apputils/Footer.tsx
+++ src/apputils/Footer.tsx
@@ -5,129 +5,124 @@ function Footer() {
   const navigate = useNavigate();
 
   return (
-    <footer className="bg-background text-foreground w-full lg:pt-16 pb-10 px-8 md:px-16 mt-10">
-      <div className="w-full  mx-auto grid grid-cols-1 md:grid-cols-5 gap-16 items-center">
-        {/* Brand */}
-        <div className="flex flex-col  space-y-4 items-center">
+    <footer className="bg-background text-foreground/80 w-full lg:pt-16 pb-10 px-8 md:px-16 mt-10">
+      <div className="w-full  flex  gap-16   justify-between  ">
+        <div className="flex flex-col  space-y-4 items-center max-w-[15vw]">
           <div className="cursor-pointer  w-fit    ">
             <img src="/logo-1.png" className="w-48" alt="logo" />
           </div>
           <p className=" w-full text-center">
-            A smart, intelligent system for data, knowledge, and AI-powered reasoning.
+            A smart, intelligent system for data, knowledge, and AI-powered
+            reasoning.
           </p>
         </div>
 
         {/* Quick Links */}
-        <div className="flex flex-col items-start space-y-4">
-          <h3 className="text-lg font-semibold">Quick Links</h3>
-          <div className="  flex  items-center gap-4">
+        <div className="flex flex-col items-start space-y-4 ">
+          <h3 className="  text-foreground/60">QUICK LINKS</h3>
+          <div className="  flex  items-start flex-col gap-4">
             <button
               onClick={() => navigate("/about")}
-              className="hover:text-blue-300 transition duration-300 text-left"
+              className="cursor-pointer lg:hover:text-primary transition duration-300 text-left"
             >
               About Us
             </button>
             <button
               onClick={() => navigate("/contact")}
-              className="hover:text-blue-300 transition duration-300 text-left"
+              className="cursor-pointer lg:hover:text-primary transition duration-300 text-left"
             >
-              Contact
+              Contact Us
+            </button>
+            <button
+              onClick={() => navigate("/privacy-policy")}
+              className="cursor-pointer lg:hover:text-primary transition duration-300 text-left text-nowrap"
+            >
+              Privacy Policy
+            </button>
+            <button
+              onClick={() => navigate("/terms")}
+              className="cursor-pointer lg:hover:text-primary transition duration-300 text-left text-nowrap"
+            >
+              Terms of Service
             </button>
             <button
               onClick={() => navigate("/faq")}
-              className="hover:text-blue-300 transition duration-300 text-left"
+              className="cursor-pointer lg:hover:text-primary transition duration-300 text-left"
             >
               FAQ
             </button>
           </div>
         </div>
 
-        {/* Legal */}
         <div className="flex flex-col items-start space-y-4">
-          <h3 className="text-lg font-semibold">Legal</h3>
-          <div className="   gap-3 lg:gap-0 grid lg:grid-cols-2  items-start">
-            <button
-              onClick={() => navigate("/privacy-policy")}
-              className="hover:text-blue-300 transition duration-300 text-left text-nowrap"
-            >
-              Privacy Policy
-            </button>
-            <button
-              onClick={() => navigate("/terms")}
-              className="hover:text-blue-300 transition duration-300 text-left text-nowrap"
-            >
-              Terms of Service
-            </button>
-
-            <button
-              onClick={() => navigate("/cookie-policy")}
-              className="hover:text-blue-300 transition duration-300 text-left text-nowrap"
-            >
-              Cookie Policy
-            </button>
-            
-            <button
-              onClick={() => navigate("/faq")}
-              className="hover:text-blue-300 transition duration-300 text-left text-nowrap"
-            >
-              Faqs
-            </button>
-
-
-            <button
-              onClick={() => navigate("/refund-policy")}
-              className="hover:text-blue-300 transition duration-300 text-left text-nowrap"
-            >
-              Refund Policy
-            </button>
-            
+          <h3 className="  text-foreground/60">DEVELOPERS</h3>
+          <div className="   gap-3 flex flex-col  items-start">
             <button
               onClick={() => navigate("/api-policy")}
-              className="hover:text-blue-300 transition duration-300 text-left text-nowrap"
+              className="cursor-pointer lg:hover:text-primary transition duration-300 text-left text-nowrap"
             >
               API Policy
             </button>
             <button
               onClick={() => navigate("/developer-policy")}
-              className="hover:text-blue-300 transition duration-300 text-left text-nowrap"
+              className="cursor-pointer lg:hover:text-primary transition duration-300 text-left text-nowrap"
             >
               Developer Policy
             </button>
+          </div>
+        </div>
+
+        <div className="flex flex-col items-start space-y-4">
+          <h3 className="  text-foreground/60">COMPANY</h3>
+          <div className="   gap-3 flex flex-col  items-start">
+            
+
+            <button
+              onClick={() => navigate("/cookie-policy")}
+              className="cursor-pointer lg:hover:text-primary transition duration-300 text-left text-nowrap"
+            >
+              Cookie Policy
+            </button>
+
+
+            <button
+              onClick={() => navigate("/refund-policy")}
+              className="cursor-pointer lg:hover:text-primary transition duration-300 text-left text-nowrap"
+            >
+              Refund Policy
+            </button>
+
             <button
               onClick={() => navigate("/data-usage-policy")}
-              className="hover:text-blue-300 transition duration-300 text-left text-nowrap"
+              className="cursor-pointer lg:hover:text-primary transition duration-300 text-left text-nowrap"
             >
               Data Usage Policy
             </button>
             <button
               onClick={() => navigate("/security-policy")}
-              className="hover:text-blue-300 transition duration-300 text-left text-nowrap"
+              className="cursor-pointer lg:hover:text-primary transition duration-300 text-left text-nowrap"
             >
               Security Policy
             </button>
-            
+
             <button
               onClick={() => navigate("/compliance-policy")}
-              className="hover:text-blue-300 transition duration-300 text-left text-nowrap"
+              className="cursor-pointer lg:hover:text-primary transition duration-300 text-left text-nowrap"
             >
               Compliance Policy
             </button>
-            
+
             <button
               onClick={() => navigate("/legal-notice")}
-              className="hover:text-blue-300 transition duration-300 text-left text-nowrap"
+              className="cursor-pointer lg:hover:text-primary transition duration-300 text-left text-nowrap"
             >
               Legal Notice
             </button>
-
-
-
           </div>
         </div>
 
-        {/* Social Media */}
-        <div className="flex flex-col items-start space-y-4">
-          <h3 className="text-lg font-semibold">Connect With Us</h3>
+        <div className="flex flex-col items-start space-y-4 ">
+          <h3 className="text-foreground/60">Connect With Us</h3>
           <div className="flex gap-6 text-2xl">
             <button
               onClick={() => navigate("/twitter")}
@@ -158,7 +153,7 @@ function Footer() {
       {/* Bottom Section */}
       <div className="w-full flex flex-col md:flex-row justify-between items-center mt-5 border-t border-white/20 text-center  text-foreground gap-2">
         <p>© 2025 Cortexvia. All rights reserved.</p>
-        
+
         <div className="flex space-x-6 lg:mt-4 md:mt-0">
           <button
             onClick={() => navigate("/twitter")}
