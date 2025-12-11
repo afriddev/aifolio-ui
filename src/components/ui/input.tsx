31ffbca95971450f8c4bb11937a1508da3596c00
harshidev01 <harshithavenkat017@gmail.com>
2025-12-11 15:16:23 +0530

    updated bg for the inputs in login and signup and some changes in phoneInput

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -50,7 +50,7 @@ const Input = React.forwardRef<HTMLInputElement, InputInterface>(
               type={type}
               data-slot="input"
               className={cn(
-                "file:text-foreground placeholder:text-muted-foreground selection:bg-secondary selection:text-primary-foreground dark:bg-input/30 border-input flex h-9 w-full min-w-0 rounded-sm border bg-transparent px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file: file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md: ",
+                "file:text-foreground placeholder:text-muted-foreground selection:bg-secondary selection:text-primary-foreground dark:bg-input/30 border-input flex h-9 w-full min-w-0 rounded-sm border bg-white px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file: file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md: ",
                 "focus-visible:border-secondary focus-visible:ring-secondary/80 focus-visible:ring-[1px]",
                 "aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
                 `${icon && "pl-10"}`,
